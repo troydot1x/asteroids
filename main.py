4fc575d4637fd552dd206b65908f7db8fffb512c
troydot1x <troydot1x@gmail.com>
2025-04-26 00:22:03 -0400

    adding the constants file

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import pygame
+from constants import *
 
-#need to check something
 
 def main():
-    print("Starting Asteroids!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        color = (0, 0, 0)
+        screen.fill(color)
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+    
+    #print("Starting Asteroids!")
+    #print(f"Screen width: {SCREEN_WIDTH}")
+    #print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
